99406a954ea004cf77e0afbcc378d0bfb66c1c30
Wolverine <95951902+WolverinexD@users.noreply.github.com>
2021-12-23 12:54:34 +0530

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -82,7 +82,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-*Hello {first_name} !* 𝐈 𝐀𝐦 𝐋𝐢𝐯𝐯𝐲🥀☘, 𝐀 𝐌𝐨𝐝𝐮𝐥𝐚𝐫 𝐆𝐫𝐨𝐮𝐩 𝐌𝐚𝐧𝐚𝐠𝐞𝐫 𝐁𝐨𝐭. [💫](https://telegra.ph/file/bef15d8b49003bc749b66.jpg)
+*Hello!* 𝐈 𝐀𝐦 𝐋𝐢𝐯𝐯𝐲🥀☘, 𝐀 𝐌𝐨𝐝𝐮𝐥𝐚𝐫 𝐆𝐫𝐨𝐮𝐩 𝐌𝐚𝐧𝐚𝐠𝐞𝐫 𝐁𝐨𝐭. [💫](https://telegra.ph/file/bef15d8b49003bc749b66.jpg)
 ________________
 × *Uptime:* `{uptime}`
 × `{sql.num_users}` *users, across* `{sql.num_chats(}` *chats.*
